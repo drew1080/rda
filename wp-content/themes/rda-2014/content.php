89cf7511d215edaaa72fc93c8fbe48466ba3d0da
Andrew Cobb <cobb.andrew@gmail.com>
2014-05-08 12:14:23 -0700

    Add favicons.  Add MailChimp

--- wp-content/themes/rda-2014/content.php
+++ wp-content/themes/rda-2014/content.php
@@ -53,7 +53,7 @@
 		<?php endif; // is_single() ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( (is_search())) : // Only display Excerpts for Search ?>
+	<?php if ( ( is_search() || is_home() )) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
